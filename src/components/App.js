4b27b36068f86a3183f8951ea6bd1e824d04dde2
Win Feigle <win.feigle@gmail.com>
2022-07-19 21:54:08 -0600

    commit tree 2d104a037c5fe6b6cbdef109b5d20007d5968617

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [imgSrc, setImgSrc] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {
+                setImgSrc(data.message);
+                setIsLoading(false);
+            });
+    },[]);
+
+    if (isLoading) return <p>Loading...</p>;
+
+    return(
+        <div>
+            <img src={imgSrc} alt="Dog"/>
+        </div>
+    );
+}
+
+export default App
